d9a3ac68532940f2055b95471783622aaade64a5
Chris Huber <chubes4@users.noreply.github.com>
2026-01-19 18:45:49 -0600

    Add user badges CSS with color variables

--- inc/assets.php
+++ inc/assets.php
@@ -7,62 +7,90 @@
  * @package ExtraChill\Users
  */
 
-if (!defined('ABSPATH')) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
 /**
  * Enqueue avatar menu and online users assets.
  */
 function extrachill_users_enqueue_avatar_menu_assets() {
-    $css_path = EXTRACHILL_USERS_PLUGIN_DIR . 'assets/css/avatar-menu.css';
-    if (file_exists($css_path)) {
-        wp_enqueue_style(
-            'extrachill-users-avatar-menu',
-            EXTRACHILL_USERS_PLUGIN_URL . 'assets/css/avatar-menu.css',
-            array(),
-            filemtime($css_path),
-            'all'
-        );
-    }
+	$css_path = EXTRACHILL_USERS_PLUGIN_DIR . 'assets/css/avatar-menu.css';
+	if ( file_exists( $css_path ) ) {
+		wp_enqueue_style(
+			'extrachill-users-avatar-menu',
+			EXTRACHILL_USERS_PLUGIN_URL . 'assets/css/avatar-menu.css',
+			array(),
+			filemtime( $css_path ),
+			'all'
+		);
+	}
 
-    $js_path = EXTRACHILL_USERS_PLUGIN_DIR . 'assets/js/avatar-menu.js';
-    if (file_exists($js_path)) {
-        wp_enqueue_script(
-            'extrachill-users-avatar-menu',
-            EXTRACHILL_USERS_PLUGIN_URL . 'assets/js/avatar-menu.js',
-            array(),
-            filemtime($js_path),
-            true
-        );
-    }
+	$js_path = EXTRACHILL_USERS_PLUGIN_DIR . 'assets/js/avatar-menu.js';
+	if ( file_exists( $js_path ) ) {
+		wp_enqueue_script(
+			'extrachill-users-avatar-menu',
+			EXTRACHILL_USERS_PLUGIN_URL . 'assets/js/avatar-menu.js',
+			array(),
+			filemtime( $js_path ),
+			true
+		);
+	}
 
-    $online_users_css_path = EXTRACHILL_USERS_PLUGIN_DIR . 'assets/css/online-users.css';
-    if (file_exists($online_users_css_path)) {
-        wp_enqueue_style(
-            'extrachill-users-online-users',
-            EXTRACHILL_USERS_PLUGIN_URL . 'assets/css/online-users.css',
-            array(),
-            filemtime($online_users_css_path),
-            'all'
-        );
-    }
+	$online_users_css_path = EXTRACHILL_USERS_PLUGIN_DIR . 'assets/css/online-users.css';
+	if ( file_exists( $online_users_css_path ) ) {
+		wp_enqueue_style(
+			'extrachill-users-online-users',
+			EXTRACHILL_USERS_PLUGIN_URL . 'assets/css/online-users.css',
+			array(),
+			filemtime( $online_users_css_path ),
+			'all'
+		);
+	}
+
+	$user_badges_css_path = EXTRACHILL_USERS_PLUGIN_DIR . 'assets/css/user-badges.css';
+	if ( file_exists( $user_badges_css_path ) ) {
+		wp_enqueue_style(
+			'extrachill-users-badges',
+			EXTRACHILL_USERS_PLUGIN_URL . 'assets/css/user-badges.css',
+			array( 'extrachill-root' ),
+			filemtime( $user_badges_css_path ),
+			'all'
+		);
+	}
 }
-add_action('wp_enqueue_scripts', 'extrachill_users_enqueue_avatar_menu_assets');
+add_action( 'wp_enqueue_scripts', 'extrachill_users_enqueue_avatar_menu_assets' );
 
 /**
  * Register shared auth utilities script for blocks.
  */
 function extrachill_users_register_auth_utils_script() {
-    $js_path = EXTRACHILL_USERS_PLUGIN_DIR . 'assets/js/auth-utils.js';
-    if (file_exists($js_path)) {
-        wp_register_script(
-            'extrachill-auth-utils',
-            EXTRACHILL_USERS_PLUGIN_URL . 'assets/js/auth-utils.js',
-            array(),
-            filemtime($js_path),
-            true
-        );
-    }
+	$js_path = EXTRACHILL_USERS_PLUGIN_DIR . 'assets/js/auth-utils.js';
+	if ( file_exists( $js_path ) ) {
+		wp_register_script(
+			'extrachill-auth-utils',
+			EXTRACHILL_USERS_PLUGIN_URL . 'assets/js/auth-utils.js',
+			array(),
+			filemtime( $js_path ),
+			true
+		);
+	}
 }
-add_action('wp_enqueue_scripts', 'extrachill_users_register_auth_utils_script');
+add_action( 'wp_enqueue_scripts', 'extrachill_users_register_auth_utils_script' );
+
+/**
+ * Enqueue shared-tabs assets for login-register block.
+ */
+function extrachill_users_enqueue_login_register_block_assets() {
+	global $post;
+
+	if ( ! is_singular() || empty( $post->post_content ) ) {
+		return;
+	}
+
+	if ( has_block( 'extrachill/login-register', $post ) ) {
+		wp_enqueue_style( 'extrachill-shared-tabs' );
+		wp_enqueue_script( 'extrachill-shared-tabs' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'extrachill_users_enqueue_login_register_block_assets' );
