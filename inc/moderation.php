1c06337abb8e60baa66384985568813e64baab38
Chris Huber <chubes@extrachill.com>
2026-03-12 19:40:39 +0000

    Include owned artist content in spam moderation

--- inc/moderation.php
+++ inc/moderation.php
@@ -212,8 +212,79 @@ function extrachill_users_get_user_content_objects( int $user_id ): array {
 	return $objects;
 }
 
+function extrachill_users_get_owned_artist_platform_objects( int $user_id ): array {
+	$objects         = array();
+	$artist_profile_ids = get_user_meta( $user_id, '_artist_profile_ids', true );
+
+	if ( ! is_array( $artist_profile_ids ) || empty( $artist_profile_ids ) ) {
+		return $objects;
+	}
+
+	$artist_blog_id = function_exists( 'ec_get_blog_id' ) ? (int) ec_get_blog_id( 'artist' ) : 0;
+	if ( $artist_blog_id <= 0 ) {
+		return $objects;
+	}
+
+	switch_to_blog( $artist_blog_id );
+	try {
+		foreach ( $artist_profile_ids as $artist_id ) {
+			$artist_id = (int) $artist_id;
+			if ( $artist_id <= 0 || get_post_type( $artist_id ) !== 'artist_profile' ) {
+				continue;
+			}
+
+			$objects[] = array(
+				'type'      => 'post',
+				'blog_id'   => $artist_blog_id,
+				'object_id' => $artist_id,
+				'post_type' => 'artist_profile',
+			);
+
+			$link_pages = get_posts(
+				array(
+					'post_type'      => 'artist_link_page',
+					'post_status'    => 'any',
+					'posts_per_page' => -1,
+					'fields'         => 'ids',
+					'meta_key'       => '_associated_artist_profile_id',
+					'meta_value'     => (string) $artist_id,
+				)
+			);
+
+			if ( is_array( $link_pages ) ) {
+				foreach ( $link_pages as $link_page_id ) {
+					$objects[] = array(
+						'type'      => 'post',
+						'blog_id'   => $artist_blog_id,
+						'object_id' => (int) $link_page_id,
+						'post_type' => 'artist_link_page',
+					);
+				}
+			}
+		}
+	} finally {
+		restore_current_blog();
+	}
+
+	return $objects;
+}
+
 function extrachill_users_apply_spam_visibility_to_user_content( int $user_id ) {
-	$objects = extrachill_users_get_user_content_objects( $user_id );
+	$objects = array_merge(
+		extrachill_users_get_user_content_objects( $user_id ),
+		extrachill_users_get_owned_artist_platform_objects( $user_id )
+	);
+	$objects = array_values(
+		array_reduce(
+			$objects,
+			function ( $carry, $object ) {
+				$key = $object['type'] . ':' . $object['blog_id'] . ':' . $object['object_id'];
+				$carry[ $key ] = $object;
+				return $carry;
+			},
+			array()
+		)
+	);
 	$results = array(
 		'posts'    => 0,
 		'comments' => 0,
