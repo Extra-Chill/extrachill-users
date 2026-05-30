1f09027e2023f1d3f7a50dcc3c7af58df64c612d
Chris Huber <chubes@extrachill.com>
2026-05-30 14:44:56 +0000

    feat: add Feature Rollout network-admin page (closes #66)

--- extrachill-users.php
+++ extrachill-users.php
@@ -102,6 +102,7 @@ function extrachill_users_init() {
 	$initialized = true;
 	require_once EXTRACHILL_USERS_PLUGIN_DIR . 'inc/assets.php';
 	require_once EXTRACHILL_USERS_PLUGIN_DIR . 'inc/core/gating.php';
+	require_once EXTRACHILL_USERS_PLUGIN_DIR . 'inc/core/feature-rollout-admin.php';
 	require_once EXTRACHILL_USERS_PLUGIN_DIR . 'inc/admin-access-control.php';
 	require_once EXTRACHILL_USERS_PLUGIN_DIR . 'inc/core/moderation/bootstrap.php';
 	require_once EXTRACHILL_USERS_PLUGIN_DIR . 'inc/core/abilities/register.php';
