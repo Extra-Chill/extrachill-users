fe9658ad24151a4409d2a2f17e2c9dcc68f2f851
Chris Huber <chubes@extrachill.com>
2026-09-26 09:17:15 +0000

    fix: stop redirecting off-main New Post to Studio (reverts #426)

--- inc/team-members.php
+++ inc/team-members.php
@@ -11,7 +11,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 require_once __DIR__ . '/team-members/role.php';
-require_once __DIR__ . '/team-members/admin-new-post.php';
 require_once __DIR__ . '/team-experience/events.php';
 
 /**
